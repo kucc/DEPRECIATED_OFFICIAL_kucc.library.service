ecbf5ebc20e78c516ee030a5fb2ad11699c3686f
LHJ <jjs01hwang@gmail.com>
2022-06-28 14:44:58 +0900

    STYLE : Main style 변경

---
+++ src/pages/MainPage/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledBooksContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 10px;
+  width: calc(100% - 200px);
+`;
